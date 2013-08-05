17b78b2bb38994598be53f04445c2ef2539723eb
Paras Narang <narangparas@gmail.com>
2013-08-05 23:55:55 +0530

    directed graph DFS

---
+++ directedGraphDFS.cpp
@@ -0,0 +1,57 @@
+/* Paras Narang
+ * Directed Graph DFS
+ * Some part of code borrowed from geeksforgeeks
+ */
+
+#include<iostream>
+#include <list>
+using namespace std;
+ 
+class Graph{
+    int V;
+    list<int> *adj;
+    void DFSUtil(int v, bool visited[]);
+  public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    void DFS();
+};
+
+Graph::Graph(int V){
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w){
+    adj[v].push_back(w);
+}
+
+void Graph::DFSUtil(int v, bool visited[]){
+    visited[v] = true;
+    cout << v << " ";
+
+    list<int>::iterator i;
+    for(i = adj[v].begin(); i != adj[v].end(); ++i)
+        if(!visited[*i]) DFSUtil(*i, visited);
+}
+
+void Graph::DFS(){
+    bool *visited = new bool[V];
+    for(int i = 0; i < V; i++) visited[i] = false;
+
+    for(int i = 0; i < V; i++)
+        if(visited[i] == false) DFSUtil(i, visited);
+}
+
+int main(){
+    int vertices, edges , a, b; 
+    cin >> vertices >> edges;
+    Graph g(vertices);
+    for(int i = 0; i < edges; i++){
+        cin >> a >> b;
+        g.addEdge(a, b);
+    }
+    g.DFS();
+    cout << endl;
+    return 0;
+}
